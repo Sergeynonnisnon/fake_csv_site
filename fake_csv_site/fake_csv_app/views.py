1dba51452a6c164eeae5ccb98872edb77100e424
nonnisnon <nonnisnon@gmail.com>
2021-08-29 12:59:52 +0300

    views basic structure add

--- fake_csv_site/fake_csv_app/views.py
+++ fake_csv_site/fake_csv_app/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 def login(response):
     return HttpResponse('login page be here')
 
-def
+def data_schemas(response):
+    return HttpResponse('data schemas user be here')
+
+def new_schema(response):
+    return HttpResponse('new schema be here')
+
+def data_sets(response):
+    return HttpResponse('data sets be here')
