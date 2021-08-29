a66de56075c7dca79936d9780458daad4017fcce
nonnisnon <nonnisnon@gmail.com>
2021-08-29 14:21:45 +0300

    views upgrade

--- fake_csv_site/fake_csv_app/views.py
+++ fake_csv_site/fake_csv_app/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def login(response):
     return HttpResponse('login page be here')
+
 
 def data_schemas(response):
     return HttpResponse('data schemas user be here')
 
+
 def new_schema(response):
     return HttpResponse('new schema be here')
+
 
 def data_sets(response):
     return HttpResponse('data sets be here')
