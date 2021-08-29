e45214ffb468cdfba67e618959b0ea136eae278d
nonnisnon <nonnisnon@gmail.com>
2021-08-29 12:54:16 +0300

    App add

---
+++ fake_csv_site/fake_csv_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def login(response):
+    return HttpResponse('login page be here')
+
+def
--- fake_csv_site/fake_csv_site/urls.py
+++ fake_csv_site/fake_csv_site/urls.py
@@ -1,4 +1,4 @@
-"""fake_scv_site URL Configuration
+"""fake_csv_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
